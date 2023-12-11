42b7eed9fba7ffe899600092c98ffeed6facf048
JCTSC <jctscamargo@gmail.com>
2023-12-11 11:23:39 -0300

    Detalhe
    O detalhe foi totalmente finalizado e feito com sucesso, agora vou estilizar.

--- src/Detalhe.jsx
+++ src/Detalhe.jsx
@@ -1,10 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useEffect } from "react";
 import Header from "./componentes/Header";
 import Footer from "./componentes/Footer";
 
 
 export default function Detalhe(){
+  const {id} = useParams();
+    const videos = JSON.parse(localStorage.getItem("Lista")) || [];
+    const video = videos.filter((objeto) => {
+      if(objeto.id == id) {
+        return objeto
+      }
+      else {
+        return null }
+    })
     return (
       <div>
         <Header />
@@ -12,11 +21,15 @@ export default function Detalhe(){
         <div>
             <div class="card">
             <div class="ratio ratio-16x9">
-            <iframe src={"https://www.youtube.com/embed/"+ video.link.slice(17) }frameborder="0" ></iframe>
+            <iframe src={"https://www.youtube.com/embed/"+ video[0].link.slice(17) }frameborder="0" ></iframe>
             </div>
             <div class="card-body">
-            <p class="card-text">{video.artista}</p>
-                <Link to={"/detalhe/" + video.id}>detalhe</Link>
+            <p class="card-text">{video[0].artista}</p>
+            <p class="card-text">{video[0].canal}</p>
+            <p class="card-text">{video[0].data}</p>
+            <p class="card-text">{video[0].titulo}</p>
+            <p class="card-text">{video[0].letra}</p>
+            
             </div>
             </div>
         </div>
--- src/componentes/Card.jsx
+++ src/componentes/Card.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
-
 export default function Card({video}){
     return (
+
         <div>
             <div class="card">
             <div class="ratio ratio-16x9">
